905dfe8f366e292015a28643fa30e9758ea48237
Pheeleex <ohemufelix@gmail.com>
2024-07-14 07:51:10 +0100

    fix slider

--- Components/CustomSlider.jsx
+++ Components/CustomSlider.jsx
@@ -7,52 +7,42 @@ const dotsContainerStyles = "absolute bottom-4 w-full flex justify-center top-[7
 const dotStyle = "mx-1 cursor-pointer w-2 h-2 rounded-full bg-gray-400";
 const activeDotStyle = "bg-red-600";
 
-const CustomSlider = ({ items, width, height, slideImgClass, slideContClass, largeCont, imagesPerSlide = 1 }) => {
+const CustomSlider = ({ items, width, height, slideImgClass, slideContClass, largeCont }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const groupedItems = [];
-  for (let i = 0; i < items.length; i += imagesPerSlide) {
-    groupedItems.push(items.slice(i, i + imagesPerSlide));
-  }
+ 
 
   // Change image every 5 seconds
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % groupedItems.length);
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
     }, 5000);
 
     return () => clearInterval(interval);
-  }, [groupedItems.length]);
+  }, [items.length]);
 
   const goToSlide = (index) => {
     setCurrentIndex(index);
   };
 
   return (
-    <div className={`home-slider ${largeCont} flex-1 basis-[45%] h-full relative`}>
+    <div className={`home-slider ${largeCont} h-full w-full relative`}>
       <div className={`image ${slideContClass} flex w-full h-full overflow-hidden`}>
-        {groupedItems.map((group, index) => (
-          <div
-            key={index}
-            className={`slide-img ${slideImgClass} flex w-full  transition-transform duration-300 ease-in-out`}
-            style={{ transform: `translateX(${-100 * currentIndex}%)` }}
+      {
+          items.map((url, index) => (
+            <Image src={items[currentIndex]} alt={`Slide ${index + 1}`}
+            className={`slide-img ${slideImgClass}  w-full h-[28rem] object-cover`}
+            style={{translate: `${-100 * currentIndex}%`}}
             aria-hidden={currentIndex !== index}
-          >
-            {group.map((url, imgIndex) => (
-              <Image
-                key={imgIndex}
-                src={url}
-                alt={`Slide ${index + 1} - Image ${imgIndex + 1}`}
-                className="w-full h-full object-cover"
-                width={width}
-                height={height}
-              />
-            ))}
-          </div>
-        ))}
+            key={index}
+            width={width}
+            height={height}
+        />
+          ))
+        }
       </div>
       <div className={dotsContainerStyles}>
-        {groupedItems.map((_, index) => (
+        {items.map((_, index) => (
           <div
             key={index}
             onClick={() => goToSlide(index)}
